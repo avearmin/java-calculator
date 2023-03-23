2d87eed7b11776caf40d06d77aaafa098312d6c6
avearmin <arminradoncic1@gmail.com>
2023-03-22 23:12:56 -0400

    Refactor logic for accepting and validating inputs
    This commit was made to make the function 'promptUserForCalculatorType'
    easier to read by moving the input validation expression into its own
    function named 'validateUserInput'.
    
    This commit also moves the creation of a scanner object, and its closing
    into the main block so future methods will be able to use the scanner
    to accept user input.

--- MyCalculator.java
+++ MyCalculator.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 
 public class MyCalculator {
     
-    public static String promptUserForCalculatorType() {
-        String choice;
+    public static String promptUserForCalculatorType(Scanner scanner) {
+        String calculatorType;
         
-        Scanner userInput = new Scanner(System.in);
         while(true) {
             System.out.println("Enter the calculator mode: Standard/Scientific?");
-            choice = userInput.nextLine();
-            if (choice.toLowerCase().equals("standard") || choice.toLowerCase().equals("scientific")) {
+            calculatorType = scanner.nextLine();
+            if (validateCalculatorType(calculatorType)) {
                 break;
             }
             System.out.println("Please enter a valid calculator type.");
         }
-        userInput.close();
-        return choice.toLowerCase();
+        return calculatorType;
+    }
+
+    public static boolean validateCalculatorType(String calculatorType) {
+        return calculatorType.equals("standard") || calculatorType.equals("scientific");
     }
     
     public static void main(String[] args) {
-        final String calculatorType = promptUserForCalculatorType();
+        Scanner userInput = new Scanner(System.in);
+        
+        final String calculatorType = promptUserForCalculatorType(userInput);
+        
+        userInput.close();
     }
 }
