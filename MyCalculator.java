bb7c3c6dadf1a2a380e769435ec1ecc939db71c4
avearmin <arminradoncic1@gmail.com>
2023-03-22 22:12:43 -0400

    Create function 'promptUserForCalculatorType()'

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 
 public class MyCalculator {
-    public static void main(String[] args) {
+    
+    public static String promptUserForCalculatorType() {
+        String choice;
         
+        Scanner userInput = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter the calculator mode: Standard/Scientific?");
+            choice = userInput.nextLine();
+            if (choice.toLowerCase().equals("standard") || choice.toLowerCase().equals("scientific")) {
+                break;
+            }
+            System.out.println("Please enter a valid calculator type.");
+        }
+        userInput.close();
+        return choice.toLowerCase();
+    }
+    
+    public static void main(String[] args) {
+        final String calculatorType = promptUserForCalculatorType();
     }
 }
